112a613bdc72fd7133413fc945dcee2feb708582
prince1998 <priyanshramnani21@gmail.com>
2021-03-10 01:50:10 +0530

    Finding using Mongoose

--- Connecting-Mongoose-to-Mongo/README.md
+++ Connecting-Mongoose-to-Mongo/README.md
@@ -8,7 +8,7 @@ Default Mongo DB port is 27017
 
 URI -  `mongodb://localhost:27017/db-name`
 
-# Our first Mongoose Model
+## Our first Mongoose Model
 
 In `mongoose.model('Movie',movieSchema)` Movie is the model name. Model name must start with a capital letter and it should be singular. Collection name will be movies.
 
@@ -17,8 +17,23 @@ Eg: `amadeus.save()`
 
 Inserting one requires using `collection.save()`
 
-# Insert Many
+## Insert Many
 
 Insert Many takes time. So, use insert many with callback or promise.
 If inserting many, we dont require to use `collection.save()`
+
+## Finding with Mongoose
+
+`Model.find()` returns a query object
+
+`Model.find({}).then(data => console.log(data))` will print all the data coming from Mongo
+
+Can query by certain properties
+Eg: `Movie.find({rating: 'PG-13'}).then(data => console.log(data))` will print all entries in which rating = PG-13
+
+`Model.findOne()` returns one entry
+
+`Movie.findById('ID').then(m => console.log(m))` allows to find by ID
+
+Eg: `Movie.findById('604648d93dc2df0889ecfcb2').then(m => console.log(m))`
 
--- Connecting-Mongoose-to-Mongo/index.js
+++ Connecting-Mongoose-to-Mongo/index.js
@@ -1,3 +1,4 @@
+//Connection
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/moviesDB', {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => {
@@ -8,7 +9,7 @@ mongoose.connect('mongodb://localhost:27017/moviesDB', {useNewUrlParser: true, u
     console.log(err)
 })
 
-
+//Schema
 const movieSchema = new mongoose.Schema({
     title: String,
     year: Number,
@@ -16,12 +17,21 @@ const movieSchema = new mongoose.Schema({
     rating: String
 });
 
-// const Movie = mongoose.model('Movie',movieSchema); //Model class called Movie
+//Creating the model
+const Movie = mongoose.model('Movie',movieSchema); //Model class called Movie
 // const amadeus = new Movie({title: 'Amadeus', year: 1986, score: 9.2, rating: 'R'});
 
-Movie.insertMany([
-    
-])
+//Inserting Many
+// Movie.insertMany([
+//     {title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
+//     {title: 'Alien', year: 1979, score: 9.3, rating: 'R' },
+//     {title: 'The Iron Giant', year: 1989, score: 5.3, rating: 'PG' },
+//     {title: 'Stand by me', year: 2003, score: 6.3, rating: 'PG-13' },
+// ])
+// .then(data => {
+//     console.log("It worked!");
+//     console.log(data);
+// })
 
 
 
